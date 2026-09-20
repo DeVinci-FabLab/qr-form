0886e8f054754a4fc3bcdaedd82a7dca1209ba36
Clément Dml <88406955+Twilights-0@users.noreply.github.com>
2026-09-20 15:15:21 +0200

    Add Flask app with redirect to FORM_URL

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, redirect
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def qr():
+    form_url = os.getenv('FORM_URL', 'https://example.com')
+    return redirect(form_url, code=302)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
